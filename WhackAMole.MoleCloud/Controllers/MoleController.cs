3f48a5e9a606314a631a5b5b3386c5fbb48b48ae
MTC Minneapolis Microsoft <mtcminneapolis@live.com>
2019-01-31 12:40:05 -0600

    green

--- WhackAMole.MoleCloud/Controllers/MoleController.cs
+++ WhackAMole.MoleCloud/Controllers/MoleController.cs
@@ -19,8 +19,8 @@ namespace WhackAMole.MoleCloud.Controllers
         private int _count = START;
 
        //private const string HEX_COLOR = "FF691E"; //ORANGEISH
-      private const string HEX_COLOR = "7FC9FF"; //BLUEISH
-       // const string HEX_COLOR = "00FF21"; //GREENISH
+      //private const string HEX_COLOR = "7FC9FF"; //BLUEISH
+       const string HEX_COLOR = "00FF21"; //GREENISH
 
 
         [HttpGet]
